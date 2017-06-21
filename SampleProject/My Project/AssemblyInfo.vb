dece27d69a944533d8ae5a1588c8d6bb3cb5b780
ccortes <ccortes@LVOTT1276>
2017-06-21 11:42:33 -0400

    Add project files.

---
+++ SampleProject/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("SampleProject")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SampleProject")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d6fc16d8-b2fc-4e85-abc7-1456c2839e01")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SampleProject/Views/Home/Index.vbhtml
@@ -0,0 +1,19 @@
+﻿@Code
+    ViewData("Title") = "Index"
+End Code
+
+<h2>Index</h2>
+
+<p>This is a sample project to practice how to add/change/commit code changes to the Git Hub plataform.</p>
+
+<p>Feel free to add or change anything here.</p>
+
+<div>
+    <a id="hypHome" href="http://www.leevalley.com" class="BlueUnder">
+        <img src="\Images\LVTLogo.gif" width="280" height="33" alt="Lee Valley and veritas" />
+    </a>
+</div>
+<div>
+     <h2>Today is :</h2>@DateTime.Today
+</div>
+
